bc00eea0fd8b8b113da4bfbc83f01164cd513582
avad <avad@itu.dk>
2026-04-14 13:44:31 +0200

    add generato r using networkx for graph generation

--- kattis_problem/floodingsubmarine/generators/example_generator.py
+++ kattis_problem/floodingsubmarine/generators/example_generator.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python3
+import networkx as nx
 import random
 import sys
+#import matplotlib.pyplot as plt
 
-# Init seed with first argument
-random.seed(int(sys.argv[1]))
+rand_seed = 42 #int(sys.argv[1]) #using 42 as standard its also used elsewhere
+random.seed(rand_seed)
 
-# Read the second... arguments as ints Example call:
-# example.py {seed} 1 2 3 4
-values = list(map(int, sys.argv[2:]))
+n = 4
+e = 15
+h = 3
+p = 2
 
-# Shuffle the list
-random.shuffle(values)
+#G = nx.complete_graph(n) #creates a fully connected graph (could be good for a worst case testcase)
+#G = nx.gnm_random_graph(n,e) # creates random connected graph
+G = nx.random_lobster(n, 0.5, 0.5, rand_seed)
 
-# Print in standard format, i.e. one line with the number of values,
-# followed by the space-separated values.
-print(len(values))
-print(*values)
+random_nodes = random.sample(list(G.nodes()), h)
+
+# Print number of nodes (n), edges (e), holes (h), and gates/plugs (p)
+# followed by e lines defining the edges, and then h lines defining the holes
+print(G.number_of_nodes(), G.number_of_edges(), h, p)
+#print edges
+for e in G.edges:
+    v1, v2 = e
+    print(v1,v2)
+print(*random_nodes, sep='\n')
+
+
+#Visual representation of the graph
+#nx.draw(G, with_labels=True, node_color='lightblue', edge_color='gray')
+#plt.show()
