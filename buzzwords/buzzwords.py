f09c71cf9029298161ecb83cccfe9c28aebd1eda
Nikki <nikki.s.risager@gmail.com>
2026-05-04 08:42:28 +0200

    Cleaned up code

--- buzzwords/buzzwords.py
+++ buzzwords/buzzwords.py
@@ -1,55 +1,44 @@
 from collections import defaultdict
-import time
 from functools import cache
-import math
+
+p = 31
+mod = 805306457
 
 def getHash(char: chr):
     return ord(char)
 
-startTime = time.time() 
-
-p = 31
-
 @cache
 def getPow(power: int):
-    return (p ** power) % 805306457
+    return (p ** power) % mod
 
-inputLine = input().replace(" ", "")
+while (True):
+    inputLine = input().replace(" ", "")
+    if inputLine == "": # stop at end of file
+        break
 
-# for each line in the input
-while (inputLine != ""):
-    output = [] 
     initialHash = 0
 
-    # for each posible size of the substring
+    # for each posible size of substring
     for size in range(1,len(inputLine)):
-        totalHash = 0
         buzzwords = defaultdict(int)
 
         # set initial hash
-        initialHash = (initialHash * p + getHash(inputLine[size-1])) % 805306457 # initial hash is equal to the last enitial hash, but added a extra character
+        initialHash = (initialHash * p + getHash(inputLine[size-1])) % mod # initial hash is equal to the last enitial hash, but added a extra character
         totalHash = initialHash 
         
         buzzwords[totalHash] += 1
 
-        counter = 0
-        # for each posibble starting position of the substring
+        # for each possible starting position of the substring
         for i in range(1, len(inputLine) - (size-1)):
-            counter += 1
-            # move the hash one to the right by first removing the value of the left most value, times it all by p, add the right most ellemtnt and modulo it by a constant
-            totalHash = ((totalHash - getHash(inputLine[i-1]) * getPow(size - 1)) * p + getHash(inputLine[ i + size -1])) % 805306457 
+            # move the hash one to the right
+            totalHash = (totalHash - getHash(inputLine[i-1]) * getPow(size - 1)) % mod # removing value of leftmost char 
+            totalHash = (totalHash * p) % mod # multiply by p
+            totalHash = (totalHash + getHash(inputLine[i + size -1])) % mod # add the new right most element
             buzzwords[totalHash] += 1
 
-        maxAccurences = 0
-        for value in buzzwords.values():
-            maxAccurences = max(maxAccurences, value)
-
+        maxAccurences = max(buzzwords.values())
         if maxAccurences <= 1:
             break
 
-        output.append(f"{maxAccurences}\n")
-    
-    print("".join(output))
-    inputLine = input().replace(" ", "")
-
-print("Time:", time.time() - startTime)
+        print(maxAccurences)
+    print()
