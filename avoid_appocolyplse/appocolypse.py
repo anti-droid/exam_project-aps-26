52a655d48ffe4bab0efb5bcc9e4032e5f77b953a
avad <avad@itu.dk>
2026-05-05 13:33:24 +0200

    modified flow from coursepage removed unneccecary rebuid of graph by changing original garph to be the same datatype as the actual graph. This could be done because we are ensured from the description that vertex [v][u] is uniqe, meaning that we dont need +=p, just =p. Also removed mincap calulations because it will not save time in this instance since g <= 100

--- avoid_appocolyplse/appocolypse.py
+++ avoid_appocolyplse/appocolypse.py
@@ -1,72 +1,50 @@
 from collections import defaultdict
 import sys
-
 sys.setrecursionlimit(10**6)
 
-def dfs(graph,u,dest,mincap,seen): # returns path to dest
+def dfs(graph,u,dest,seen):
     if u in seen:
         return (False,seen)
     seen.add(u)
     for v,cap in graph[u].items():
-        if cap > mincap: # only consider edges with capacity > mincap
+        if cap > 0:
             if v == dest:
                 return (True,[(u,v)])
-            #print(f'explore {u} {v}, {cap}')
-            suc, p = dfs(graph,v,dest,mincap,seen)
+            suc, p = dfs(graph,v,dest,seen)
             if suc:
                 p.append((u,v))
                 return (True,p)
     return (False,seen)
 
-
-def flow(orggraph, src,dest):
-    graph = defaultdict(lambda: defaultdict(int))
-    maxcapacity = 0
-    for u,d in orggraph.items():
-        for v,c in d.items():
-            graph[u][v] = c
-            maxcapacity = max(maxcapacity,c)
-
+def flow(graph, src,dest):
     current_flow = 0
-    mincap = maxcapacity # set to 0 to disable capacity scaling
     while True:
-        #ispath, p_or_seen = bfs(graph,src,dest,mincap)
-        ispath, p_or_seen = dfs(graph,src,dest,mincap, set())
+        ispath, p = dfs(graph,src,dest, set())
         if not ispath:
-            if mincap > 0:
-                mincap = mincap // 2
-                continue
-            else:
-                return (current_flow,
-                        { a:{b:c-graph[a][b] for b,c in d.items() if graph[a][b]<c} 
-                            for a,d in orggraph.items() },
-                        p_or_seen)
-        p = p_or_seen
-        #print("path:", *reversed(p))
+            return (current_flow)
         saturation = min( graph[u][v] for u,v in p )
-        #print(current_flow,saturation)#,[f"{u[0]}-{u[1]}:{orggraph[u[0]][u[1]]}:{graph[u][v]}" for u,v in p if u[2]==0])
         current_flow += saturation
         for u,v in p:
             graph[u][v] -= saturation
             graph[v][u] += saturation
 
-inf = 100
+
 for testcase in range(int(input())):
     n = int(input())
     i, g, s = map(int,input().split())
     sink = (n+1,s+1)
     source = (0,-1)
-    Graph = defaultdict(lambda: defaultdict(lambda: int)) 
+    Graph = defaultdict(lambda: defaultdict(int)) 
     
     Graph[source][(i,0)] = g
     for j in range(1,n+1):
         for ts in range(s):
-            Graph[(j,ts)][j,ts+1] = inf
+            Graph[(j,ts)][j,ts+1] = g
 
     m = int(input())
     for j in range(m):
         med = int(input())
-        Graph[(med,s)][sink] = inf
+        Graph[(med,s)][sink] = g
     
     r = int(input())
     for j in range(r):
@@ -74,5 +52,5 @@ for testcase in range(int(input())):
         for ts in range(s+1-t):
             Graph[(a,ts)][(b,ts+t)] = p
 
-    flow_value, residual_graph, _ = flow(Graph, source, sink)
+    flow_value = flow(Graph, source, sink)
     print(flow_value)
