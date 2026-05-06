eae9c533ca616648ff4a5a9ad12bb1eb215ee55b
Nikki <nikki.s.risager@gmail.com>
2026-05-06 09:27:12 +0200

    Added tests

--- kattis_problem/floodingsubmarine/generators/example_generator.py
+++ kattis_problem/floodingsubmarine/generators/example_generator.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 rand_seed = int(sys.argv[1]) #using 42 as standard its also used elsewhere
 random.seed(rand_seed)
 
-n = 4
+n = 10000
 e = 15
-h = 3
-p = 2
+h = 50
+p = 3
 
 #G = nx.complete_graph(n) #creates a fully connected graph (could be good for a worst case testcase)
 #G = nx.gnm_random_graph(n,e) # creates random connected graph
-G = nx.random_lobster(n, 0.5, 0.5, rand_seed)
+#G = nx.random_lobster_graph(n, 0.5, 0.5, rand_seed)
+G = nx.random_labeled_tree(n)
 
 random_nodes = random.sample(list(G.nodes()), h)
 
