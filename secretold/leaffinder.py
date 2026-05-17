5e0054c95c1e181f9e2ce61717fd760aadf96dda
Ask Kildelund <ask.kildelund@protonmail.com>
2026-05-17 10:40:09 +0200

    Inputmappe_good

--- secretold/leaffinder.py
+++ secretold/leaffinder.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 n, e, h, p = map(int,input().split())
-print(n,e,1000,950)
+print(n,e,500,500)
 G = defaultdict(lambda: 0)
 for i in range(e): # load graph
     v1, v2 = map(int, input().split())
@@ -13,5 +13,5 @@ for key in G:
     if G[key]==1:
         print(key)
         i+=1
-    if i==1000:
+    if i==500:
         break
